1a96b8e8bd17cdf7c27bb37e7f11507209089385
Andrii Radkevych <radkevychandrii7@gmail.com>
2024-07-07 13:53:20 +0300

    static-files-serving

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static('static'));
+app.use('/pages', express.static('pages'))
+
 // sendFile will go here
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port);
